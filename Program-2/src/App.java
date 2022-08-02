dbcab4edac373d32ae3193271f1207307b48c926
Naveenkumar156 <nk1956350@gmail.com>
2022-08-02 12:13:33 +0530

    Added program 2 to the repo

---
+++ Program-2/src/App.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class App 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        Scanner input = new Scanner(System.in);
+        int limit = input.nextInt();
+        printOddNumber(limit);
+        input.close();
+    }
+
+    public static void printOddNumber(int limit)
+    {
+        int count = 0;
+        for(int i = 1; true; i++)
+        {
+            if(i % 2 != 0)
+            {
+                System.out.print(i);
+                System.out.print(" ");
+                count += 1;
+            }
+
+            if(count == limit){
+                break;
+            }
+        }
+    }
+}
